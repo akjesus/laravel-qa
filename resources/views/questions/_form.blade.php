5f831cc8b22ab329071b208f7700fccd8206a308
akjesus <cutytongy@gmail.com>
2019-08-12 13:48:00 -0700

    Implementing question edit form and storing to database"

---
+++ resources/views/questions/_form.blade.php
@@ -0,0 +1,22 @@
+@csrf
+                    <div class = "form-group">
+                        <label for = "title">Question Title</label>
+                        <input type = "text" name = "title" value = "{{ old('title', $questions->title) }}"class = "form-control {{ $errors->has('title') ? 'is-invalid' : ' '}}" id = "title">
+                    </div>
+                    @if ($errors->has('title'))
+                        <span class = "invalid-feedback">
+                            <strong> {{ $errors->first('title') }} </strong>
+                        </span>
+                    @endif
+                    <div class = "form-group">
+                        <label for = "body">Explain your Question</label>
+                        <textarea name = "body" class = "form-control {{ $errors->has('body') ? 'is-invalid' : ' '}}" id = "body" rows= "6">{{ old('body',  $questions->body) }}</textarea>
+                    </div>
+                    @error('body')
+                        <span class="invalid-feedback" role="alert">
+                             <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    <div class = "form-group">
+                    <button type = "submit" class = "btn btn-outline-primary btn-lg">{{ $buttonText }}</button>
+                     </div>
--- resources/views/questions/edit.blade.php
+++ resources/views/questions/edit.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">
                 <div class = "d-flex align-items-center"> 
-                    <h3>Ask a Question</h3>
+                    <h3>Edit Question</h3>
                      
                     <div class = "ml-auto"> 
                         <a href = "{{ route('questions.index') }}" class = "btn btn-outline-secondary"> All Questions </a>
@@ -16,8 +16,11 @@
                 </div>
 
                     <div class="card-body">
-                <form action = "{{ route('questions.store') }}" method = "POST">
-                @include('questions._form',  ['buttonText' => 'Ask a Question'])
+                <form action = "{{ route('questions.update', $questions->id) }}" method = "POST">
+                    {{ method_field('PUT') }}
+
+                   @include('questions._form', ['buttonText' => 'Update Question'])
+                       
                 </form>
                     <div>
                    
